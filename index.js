5306b289a9d0c403322fc0c82daef47455c40766
Vasyl Tkachenko <vasyltkachenko07@gmail.com>
2023-08-09 15:59:32 +0300

    working version

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ mongoose
 	.connect(URL)
 	.then(() => console.log('DB ok'))
 	.catch((err) => console.log('DB error', err))
+	
 
 // const storage = multer.diskStorage({
 // 	destination: (_, __, cb) => {
